53a810ddc6e91e718713b147d1ef265d74b93c08
nishangautam <nishan.gautam.987@gmail.com>
2020-01-30 17:21:34 +0545

    hero search  add and delete also added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -24,11 +24,48 @@ export class HeroService {
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
+
   getHero(id: number): Observable<Hero> {
 const url =`${this.heroesUrl}/$id`;
-
+return this.http.get<Hero>(url).pipe(
+  tap(_=> this.log(`fetched hero id=${id}`)),
+  catchError(this.handleError<Hero>(`getHero id = ${id}`))
+);
        // this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    // return of(HEROES.find(hero => hero.id === id));
+  }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content_Type': 'application/json' })
+  };
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero: hero.id;
+    const url =`${this.heroesUrl}/$id`;
+    return this.http.delete<Hero>(url,this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+  searchHeroes(term: string): Observable<Hero[]> {
+    if(!term.trim()){
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heores matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes',[]))
+    );
   }
   private log(message: string ){
     this.messageService.add(`HeroService: ${message}`);
